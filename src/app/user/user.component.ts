0b1d3e4cdf9043fd526c3e2e35de0be084503d91
codingcurious01 <codingcurious@gmail.com>
2024-12-24 15:34:34 +0530

    added User Component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  // imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
+
+  get userImgPath(){
+    return 'assets/users/' + this.selectedUser.avatar
+  }
+
+  onSelectUser(){
+    console.log('User Clicked!')
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    this.selectedUser = DUMMY_USERS[randomIndex]
+  }
+
+}
